3abc271997c13f5f8749af3e7888945803cb2ef7
xsetiadi <exaudie@gmail.com>
2019-07-15 14:31:08 +0700

    tambah input

---
+++ app/src/main/java/com/exam/ztes/FormInput/FormInput.java
@@ -0,0 +1,15 @@
+package com.exam.ztes.FormInput;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.exam.ztes.R;
+
+public class FormInput extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_input);
+    }
+}
